9282e2c34ae9592eda884ea59ef35c0c51f260aa
carlo de gregorio <cdegregorio@gmail.com>
2015-10-13 15:25:07 -0700

    create new, update, edit links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,4 +3,44 @@ class LinksController < ApplicationController
     @links = Link.all
     render :index
   end
+
+  def show
+    @link = Link.find(params[:id])
+    render :show
+  end
+
+  def new
+    @link = Link.new
+    render :new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to links_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      redirect_to links_path
+    else
+      render :edit
+    end
+  end
+
+
+  # this is to handle potential malicious code
+  private
+  def link_params
+    params.require(:link).permit(:title, :url, :votes)
+  end
 end
