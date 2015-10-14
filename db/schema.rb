965ddba470257a08c69c52067be2b05522556438
cdg77 <cdegregorio@gmail.com>
2015-10-14 05:26:54 -0700

    has comments that have a may to one link relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013165036) do
+ActiveRecord::Schema.define(version: 20151014051136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.string   "body"
+    t.integer  "link_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
